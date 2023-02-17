e21606b26bc8b0839583d1fb4699d3646252da85
PreciousSithole <precioussithole25@gmail.com>
2023-02-16 23:06:13 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that print a line with puts fucntion
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
