432a476f17cc08a7979b7c882e6d288b808e1061
PreciousSithole <precioussithole25@gmail.com>
2023-02-28 06:54:18 -0800

    pring half of the string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *puts2 - prints every other character of a string,starting with first letter
+ *@str: string pointer
+ * Return : always 0
+ */
+void puts2(char *str)
+{
+	int length;
+	int halfstring;
+	int x;
+
+	length = 0;
+	x = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	halfstring = length / 2;
+
+	while (x <= halfstring)
+	{
+		printf("%c", str[x]);
+		x++;
+	}
+}
